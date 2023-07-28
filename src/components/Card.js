d65278ba62029a9ac55507ea9d68f947257fcb10
Gerardo J Cruz <gerardojcruz98@gmail.com>
2023-07-27 18:18:45 -0600

    ImagesGrid and deploy of Images ready

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-function Card() {
+function Card({ image }) {
+
+    // Extract card information
+    const { largeImageURL, previewURL, likes, views } = image;
+
     return (
-        <div>
-            hi
-        </div>
+        <>
+            <div className='card rounded-0'>
+                <img src={previewURL} alt="Image example" />
+                    <div className="card__info">
+                        <p className="card__likes"><span>{likes}</span> Likes</p>
+                        <p className="card__views"><span>{views}</span> views </p>
+
+                        <a className="btn card__button w-100 border-0 rounded p-1 fw-bold " href={largeImageURL} target="_blank">See the full image</a>
+                    </div>
+            </div>
+        </>
     )
 }
 
