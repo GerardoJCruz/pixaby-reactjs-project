2ca7d1dd9d4740d4db2a98e986883aad594b9313
Gerardo J Cruz <gerardojcruz98@gmail.com>
2023-07-30 20:43:57 -0600

    Corrections

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,12 +8,12 @@ function Card({ image }) {
     return (
         <>
             <div className='card rounded-0'>
-                <img src={previewURL} alt="Image example" />
+                <img src={previewURL} alt="Img" rel='noreferrer'/>
                     <div className="card__info">
                         <p className="card__likes"><span>{likes}</span> Likes</p>
                         <p className="card__views"><span>{views}</span> views </p>
 
-                        <a className="btn card__button w-100 border-0 rounded p-1 fw-bold " href={largeImageURL} target="_blank">See the full image</a>
+                        <a className="btn card__button w-100 border-0 rounded p-1 fw-bold " href={largeImageURL} target="_blank" rel='noreferrer'>See the full image</a>
                     </div>
             </div>
         </>
